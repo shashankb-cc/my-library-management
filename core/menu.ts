7e89375bd4ba08efc9f5003acedee3285425acc8
Shashank <shashank.b@codecraft.co.in>
2024-07-02 19:39:56 +0530

    Added menu.ts to separate the menu interface and also fixed issue with the book and library interactor

--- core/menu.ts
+++ core/menu.ts
@@ -1,9 +1,22 @@
 export interface IMenuItem {
   key: string;
-  lable: string;
+  label: string;
 }
-
 export class Menu {
   private items: IMenuItem[] = [];
-  constructor(items: IMenuItem[]);
+  constructor(items: IMenuItem[]) {
+    this.items = items;
+  }
+  serialize(): string {
+    return this.items.reduce((str, item) => {
+      if (str) {
+        str += "\n";
+      }
+      str += `${item.key}.\t${item.label}`;
+      return str;
+    }, "");
+  }
+  getItem(key: string) {
+    return this.items.find((i) => i.key === key) || null;
+  }
 }
