370e6cd82aac69f36f38966e95f49e701c013bd4
Shashank <shashank.b@codecraft.co.in>
2024-07-25 09:38:28 +0530

    Successfully tested the drizzle working and tested to insert the value

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,41 @@
+import {
+  bigint,
+  int,
+  mysqlTable,
+  serial,
+  varchar,
+} from "drizzle-orm/mysql-core";
+
+export const books = mysqlTable("books", {
+  id: serial("id").primaryKey(),
+  title: varchar("title", { length: 100 }).notNull(),
+  author: varchar("author", { length: 100 }).notNull(),
+  publisher: varchar("publisher", { length: 100 }).notNull(),
+  genre: varchar("genre", { length: 100 }).notNull(),
+  isbnNo: varchar("isbnNo", { length: 13 }),
+  numOfPages: int("numOfPages").notNull(),
+  totalNumOfCopies: int("totalNumOfCopies").notNull(),
+  availableNumberOfCopies: int("availableNumberOfCopies").notNull(),
+});
+
+export const members = mysqlTable("members", {
+  id: serial("id").primaryKey(),
+  firstName: varchar("firstName", { length: 100 }).notNull(),
+  lastName: varchar("lastName", { length: 100 }).notNull(),
+  email: varchar("email", { length: 100 }).unique().notNull(),
+  phoneNumber: varchar("phoneNumber", { length: 10 }).notNull(),
+});
+
+export const transactions = mysqlTable("transactions", {
+  id: serial("id").primaryKey(),
+  bookId: bigint("bookId", { mode: "bigint", unsigned: true })
+    .references(() => books.id)
+    .notNull(),
+  memberId: bigint("memberId", { mode: "bigint", unsigned: true })
+    .references(() => members.id)
+    .notNull(),
+  issueDate: varchar("issueDate", { length: 100 }).notNull(),
+  dueDate: varchar("dueDate", { length: 100 }).notNull(),
+  returnDate: varchar("returnDate", { length: 100 }),
+  Status: varchar("Status", { length: 100 }).notNull(),
+});
